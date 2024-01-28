c7253634cd3616566687f2260c634bdaec2b69a6
Demian <damien.ldvdc@gmail.com>
2024-01-28 20:03:11 -0300

    Simplification

--- app/app.py
+++ app/app.py
@@ -1,45 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
-import shutil
-import subprocess
 import secrets
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
-
-# Configuration Flask-Login
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-# Configuration de la clé secrète
-app.secret_key = secrets.token_hex(16)
 
 # Configuration des dossiers de médias
 app.config['UPLOAD_FOLDER'] = 'app/static/medias'
 app.config['THMBS_FOLDER'] = 'app/static/thmbs'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
-app.config['ARCHIVE_FOLDER'] = 'app/archive'
-
-# Classe User pour Flask-Login
-class User(UserMixin):
-    def __init__(self, user_id):
-        self.id = user_id
-
-    def is_authenticated(self):
-        return True  # Ajoutez votre logique d'authentification si nécessaire
 
 # Fonction pour vérifier les extensions autorisées
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
-# Fonction de chargement de l'utilisateur pour Flask-Login
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
 
 # Fonction pour récupérer le numéro de fichier le plus élevé dans le répertoire 'thmbs'
 def get_highest_file_number():
@@ -64,37 +38,14 @@ def generate_unique_filename(filename):
     global file_counter
     file_counter += 1
     file_extension = os.path.splitext(filename)[1].lower()
-    return f"{file_counter:04d}{file_extension}"  # Format numérique avec 4 chiffres remplis de zéros à gauche si nécessaire
+    return f"{file_counter:04d}_{secrets.token_hex(8)}{file_extension}"  # Format numérique avec 4 chiffres remplis de zéros à gauche si nécessaire
 
-# Page protégée par l'authentification
+# Page protégée
 @app.route('/')
-@login_required
 def protected():
     thmbs_folder = app.config['THMBS_FOLDER']
     images = sorted(os.listdir(thmbs_folder), key=lambda x: os.path.getmtime(os.path.join(thmbs_folder, x)), reverse=True)
-    return render_template('index.html', images=images, user=current_user)
-
-# Page de connexion
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    # Déplacez la génération du mot de passe haché à l'intérieur de cette fonction
-    hashed_password = bcrypt.generate_password_hash("votremotdepasse").decode("utf-8")
-    if request.method == 'POST':
-        password_attempt = request.form['password']
-        if bcrypt.check_password_hash(hashed_password, password_attempt):
-            user = User(user_id=1)  # Remplacez par la logique pour obtenir l'utilisateur depuis la base de données
-            login_user(user)
-            return redirect(url_for('protected'))
-        else:
-            return 'Mot de passe incorrect'
-    return render_template('login.html')
-
-# Page de déconnexion
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+    return render_template('index.html', images=images)
 
 # Téléchargement de fichiers
 @app.route('/upload', methods=['POST'])
@@ -110,7 +61,7 @@ def upload():
         # Enregistrement dans le dossier 'medias' avec le nouveau nom
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
         # Enregistrement dans le dossier 'thmbs' avec redimensionnement et le nouveau nom
-        thumbnail_path = os.path.join(app.config['THMBS_FOLDER'], f"_{new_filename}")
+        thumbnail_path = os.path.join(app.config['THMBS_FOLDER'], new_filename)
         # Ouvrir l'image avec Pillow
         original_image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], new_filename))
         # Redimensionner l'image à une largeur de 180 pixels
@@ -118,29 +69,6 @@ def upload():
         # Enregistrement de l'image redimensionnée
         resized_image.save(thumbnail_path)
     return redirect(url_for('protected'))
-
-# Route pour télécharger l'archive des médias via Transfer.sh
-@app.route('/download', methods=['GET'])
-def download_media_archive():
-    # Chemin du dossier médias
-    media_folder = app.config['UPLOAD_FOLDER']
-    
-    # Chemin de l'archive à créer dans le dossier des archives
-    zip_path = os.path.join(app.config['ARCHIVE_FOLDER'], 'medias.zip')
-
-    # Créer une archive contenant tous les fichiers du dossier médias
-    try:
-        shutil.make_archive(os.path.join(app.config['ARCHIVE_FOLDER'], 'medias'), 'zip', media_folder)
-    except Exception as e:
-        return jsonify({'error': f"Une erreur est survenue lors de la création de l'archive : {str(e)}"})
-
-    # Envoyer l'archive à Transfer.sh pour le téléchargement
-    try:
-        response = subprocess.check_output(['curl', '--upload-file', zip_path, 'https://transfer.sh/medias.zip'])
-        download_link = response.decode('utf-8').strip()
-        return jsonify({'success': True, 'download_link': download_link})
-    except subprocess.CalledProcessError as e:
-        return jsonify({'error': f"Une erreur est survenue lors de l'envoi de l'archive à Transfer.sh : {str(e)}"})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
